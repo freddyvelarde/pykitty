645c4e68fb51b238ab0eb984d1ab8fa2048e1d81
freddy velarde <freddyvelarde59@gmail.com>
2022-12-23 16:39:57 -0400

    updating to github

---
+++ README.md
@@ -0,0 +1,1 @@
+curl https://raw.githubusercontent.com/freddyvelarde/test-sh/master/install.sh | bash
--- install-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-if ! grep -q 'export PATH=$PATH:$HOME/dev/projects/CLI/theme-switcher' ~/.zshrc; then
-  sed -i '1i export PATH=$PATH:$HOME/dev/projects/CLI/theme-switcher' ~/.zshrc 
-  source ~/.zshrc || true
-# echo "Word not found in file.txt"
-else 
-  echo "pykitty already set it"
-fi
-
